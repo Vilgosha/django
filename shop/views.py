9d28a17a9b4b9aec41778928633e09ad4cb9a219
Vilgosha <gromovoiwtorm@gmail.com>
2025-08-05 16:52:13 +0300

    Module 61 Django finished

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from . import models
+from .models import Course
+
 
 # Create your views here.
 
 
+# def index(request):
+#     return HttpResponse("Hello from the Shop app")
+
+
 def index(request):
-    return HttpResponse("Hello from the Shop app")
+    courses = Course.objects.all()
+    return render(request, 'courses.html', {'courses': courses})
+    # return HttpResponse(courses)
+    # return HttpResponse(''.join([str(course) + '<br>' for course in courses]))
+
+
+# models.Course
+# models.Category
